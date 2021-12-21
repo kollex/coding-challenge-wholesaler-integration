1671c3b748be6e0a15642d87245790cabec64360
Krzysztof Kardasz <krzysztof@kardasz.eu>
2021-12-21 13:10:47 +0100

    Add Product Test

--- tests/Unit/ProductTest.php
+++ tests/Unit/ProductTest.php
@@ -11,8 +11,53 @@ class ProductTest extends TestCase
     public function testIdRequired(): void
     {
         $this->expectException(InvalidArgumentException::class);
+        $this->product()->setId('');
+    }
 
-        $subject = new Product();
-        $subject->setId('');
+    public function testManufacturerRequired(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->product()->setManufacturer('');
+    }
+
+    public function testNameRequired(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->product()->setName('');
+    }
+
+    public function testPackagingRequired(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->product()->setPackaging(null);
+    }
+
+    public function testBaseProductPackagingRequired(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->product()->setBaseProductPackaging(null);
+    }
+
+    public function testBaseProductUnitRequired(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->product()->setBaseProductUnit(null);
+    }
+
+    public function testBaseProductAmountRequired(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->product()->setBaseProductAmount(null);
+    }
+
+    public function testBaseProductQuantityRequired(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->product()->setBaseProductQuantity(null);
+    }
+
+    private function product(): Product
+    {
+        return new Product();
     }
 }
